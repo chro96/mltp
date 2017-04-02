f79537a29c7534728ad955a04ebf0f5b7d869f7c
kent fujii <kent.where.the.light.is@gmail.com>
2017-04-02 22:41:27 +0900

    fix: tab_to_indentation

--- s4_k-means/s1_exercise1.py
+++ s4_k-means/s1_exercise1.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 class MyKMeans(object):
-	def __init__(self, n_clusters=8, max_iter=300, random_state=None):
-		self.n_clusters = n_clusters
-		self.max_iter = max_iter
-		self.random_state = random_state
-		if self.random_state:
-			np.random.seed(self.random_state)
+    def __init__(self, n_clusters=8, max_iter=300, random_state=None):
+        self.n_clusters = n_clusters
+        self.max_iter = max_iter
+        self.random_state = random_state
+        if self.random_state:
+            np.random.seed(self.random_state)
 
-	def fit(self, X):
-		self.cluster_centers_ = # Your code here
-		return self
+    def fit(self, X):
+        self.cluster_centers_ = # Your code here
+        return self
 
 X = np.array([[1,1],[1,2],[2,2],[4,5],[5,4]])
 kmeans = MyKMeans(n_clusters=2, max_iter=5, random_state=1).fit(X)
@@ -25,4 +25,3 @@ assert(all(c in X for c in kmeans.cluster_centers_))
 assert(not np.array_equal(kmeans.cluster_centers_[0], kmeans.cluster_centers_[1]))
 # これはサンプルアウトプットなので上の３つが通ってればOKです。
 assert(np.array_equal(kmeans.cluster_centers_, np.array([[2,2],[1,2]])))
-
