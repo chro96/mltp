493f5b2334f9765d063bb63531aeef07c1a8884b
kent fujii <kent.where.the.light.is@gmail.com>
2017-04-10 01:27:25 +0900

    wip: s5

--- s5_practices/exercise1.py
+++ s5_practices/exercise1.py
@@ -11,20 +11,20 @@ class MyLinearRegression(object):
         self.n_iter = n_iter
 
     def fit(self, X, y):
-        X = np.insert(X, 0, 1, axis=1)
-        # X = [[1 0]
+        x = np.insert(X, 0, 1, axis=1)
+        # x = [[1 0]
         #      [1 1]
         #      [1 2]
         #      [1 3]]
         # shape: 大きさが 5 のベクトルはスカラー 5 に、 4×1 の行列はタプル (4, 2) となる
-        self.w = np.ones(X.shape[1])
-        m = X.shape[0]
+        self.w = np.ones(x.shape[1])
         for i in range(self.n_iter):
             # numpy: dotは内積(スカラー積)をする
             # θ の値は適当に入れる
             # 最終的な目的はこの θ を見つけること。
             # その為にはまず θ を適当に選び、h(x) が y とどれだけ違うかに応じて θ を修正していく
-            output = X.dot(self.w)
+            # h(X)=Xθ
+            output = x.dot(self.w)
             return output
 
 X = np.array([[0], [1], [2], [3]])
--- s5_practices/exercise2.py
+++ s5_practices/exercise2.py
@@ -2,37 +2,29 @@ from IPython import embed
 import numpy as np
 
 
-# h(X)=Xθ を計算しましょう。
 class MyLinearRegression(object):
     def __init__(self, eta=0.1, n_iter=50):
-        # eta: learning rateと呼ばれるもので、どれだけ早く最適化するかを設定する。
-        # これが小さすぎると学習が遅く、大きすぎると収束しなくなる。
         self.eta = eta
         self.n_iter = n_iter
 
     def fit(self, X, y):
-        X = np.insert(X, 0, 1, axis=1)
-        # X = [[1 0]
-        #      [1 1]
-        #      [1 2]
-        #      [1 3]]
-        # shape: 大きさが 5 のベクトルはスカラー 5 に、 4×1 の行列はタプル (4, 2) となる
-        self.w = np.ones(X.shape[1])
+        x = np.insert(X, 0, 1, axis=1)
+        self.w = np.ones(x.shape[1])
         m = X.shape[0]
         for i in range(self.n_iter):
-            # numpy: dotは内積(スカラー積)をする
-            # θ の値は適当に入れる
-            # 最終的な目的はこの θ を見つけること。
-            # その為にはまず θ を適当に選び、h(x) が y とどれだけ違うかに応じて θ を修正していく
-            # θ := θ + (α/m)(y−h(x))X
-            output = X.dot(self.w)
+            # h(x)
+            output = x.dot(self.w)
+            # (y−h(x))
             errors = y - output
-            if i % 10 == 0:
-                print("Error: ", sum(errors ** 2))
-                print("Weights: ", self.w)
-                print("-------------------------")
+            # if i % 10 == 0:
+            #     print("Error: ", sum(errors ** 2))
+            #     print("Weights: ", self.w)
+            #     print("-------------------------")
+            # θ := θ + (α/m)(y−h(x))X
             self.w += self.eta / m * errors.dot(X)
-        return self.w
+            print(self.w)
+            return self.w
+        # return self
 
 X = np.array([[0], [1], [2], [3]])
 y = np.array([-1, 1, 3, 5])
