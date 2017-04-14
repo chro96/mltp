3f8a5a5d4443347e7f0b0f4ad6625a1ae32ea4ca
kent fujii <kent.where.the.light.is@gmail.com>
2017-04-14 18:32:24 +0900

    wip: s5

--- s5_practices/exercise2.py
+++ s5_practices/exercise2.py
@@ -8,27 +8,26 @@ class MyLinearRegression(object):
         self.n_iter = n_iter
 
     def fit(self, X, y):
-        x = np.insert(X, 0, 1, axis=1)
-        self.w = np.ones(x.shape[1])
+        X = np.insert(X, 0, 1, axis=1)
+        self.w = np.ones(X.shape[1])
         m = X.shape[0]
         for i in range(self.n_iter):
             # h(x)
-            output = x.dot(self.w)
+            output = X.dot(self.w)
             # (y−h(x))
             errors = y - output
-            # if i % 10 == 0:
-            #     print("Error: ", sum(errors ** 2))
-            #     print("Weights: ", self.w)
-            #     print("-------------------------")
+            if i % 10 == 0:
+                print("Error: ", sum(errors ** 2))
+                print("Weights: ", self.w)
+                print("-------------------------")
             # θ := θ + (α/m)(y−h(x))X
             self.w += self.eta / m * errors.dot(X)
-            print(self.w)
             return self.w
-        # return self
 
 X = np.array([[0], [1], [2], [3]])
 y = np.array([-1, 1, 3, 5])
 regr = MyLinearRegression()
 weight = regr.fit(X, y)
 print(weight)
-assert(np.array_equal(weight, np.array([1.05, 1.2])))
+# 最初のイテレーションの値をテストする
+assert(np.array_equal(weight, np.array([0.95, 1.05])))
